b0fb44a331d387277cc1b249addf838658d07ca8
Alonso Avotz <alonso@avotz.com>
2017-11-14 10:52:27 -0600

    se cambio boton de paypal

--- wp-content/themes/buggytours/woocommerce/single-product/price.php
+++ wp-content/themes/buggytours/woocommerce/single-product/price.php
@@ -30,13 +30,12 @@ global $product;
       
       </a>
 
+
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="form-paypal" style="margin-top:5px;">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="9DN6WQY2E466G">
-<table>
-<tr><td><input type="hidden" name="on0" value="Amount">Amount</td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
-</table>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 
